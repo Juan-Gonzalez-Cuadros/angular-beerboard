f2930e2d25232c649c67bcff22c1152c26458088
Juan-Gonzalez <55216941+Juan-Gonzalez-Cuadros@users.noreply.github.com>
2019-12-06 20:44:35 -0600

    Create beerboard.component.ts

---
+++ src/app/components/beerboard/beerboard.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-beerboard',
+  templateUrl: './beerboard.component.html'
+})
+export class BeerboardComponent {
+
+  constructor() { }
+
+}
